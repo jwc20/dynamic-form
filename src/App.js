2a071da932c2db138d59567b4fe4bf0af379d4cf
jwc20 <choij2015@gmail.com>
2022-08-11 16:52:24 -0700

    created input field with material's TextField and Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField"
+
 function App() {
+  const [inputField, setInputField] = useState([
+    {
+      firstName: "",
+      lastName: "",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <p>Hello World</p>
-    </div>
+    <Container>
+      <h1>Add new member</h1>
+      <form>
+        {inputField.map((inputField, index) => (
+          <div key={index}>
+            <TextField
+              name="firstName" 
+              label="First Name"
+              value={inputField.firstName}
+            />
+          </div>
+        ))}
+      </form>
+    </Container>
   );
 }
 
